a15339a4a963dbdade02e7e07263681fad5b7ff4
Fakhar Mumtaz <fakharmumtaz@gmail.com>
2024-05-31 21:46:48 -0400

    Only StreamOnChar.java

--- java-projcts/all-java/src/main/java/com.javalogic.stream/StreamOnChar.java
+++ java-projcts/all-java/src/main/java/com.javalogic.stream/StreamOnChar.java
@@ -1,2 +1,7 @@
-package com.javalogic.stream;public class StreamOnChar {
+package com.javalogic.stream;
+
+public class StreamOnChar {
+    public static void main(String[] args) {
+        System.out.println("StreamOnChar");
+    }
 }
