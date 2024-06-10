0c766e046386dcd7e4be7875c3e0480f5ca0c847
Fakhar Mumtaz <fakharmumtaz@gmail.com>
2024-06-09 21:51:52 -0400

    Fibonacci

--- java-projcts/all-java/src/main/java/com/javalogic/codprob/Fibnocci.java
+++ java-projcts/all-java/src/main/java/com/javalogic/codprob/Fibnocci.java
@@ -2,9 +2,15 @@ package com.javalogic.codprob;
 
 public class Fibnocci {
     public static void main(String[] args) {
-        int res = getFibnoci1(90);
-        //System.out.println(res);
-        printFibonacci(10);
+        int num = 10;
+        printFibonacci(num);
+        System.out.println();
+        printFibonacci1(num);
+        System.out.println();
+        for(int i = 0; i < num; i++ ){
+            int res = printFibonacci2(i);
+            System.out.println(res);
+        }
     }
 
     private static void printFibonacci(int num) {
@@ -17,14 +23,21 @@ public class Fibnocci {
         }
     }
 
-    public static int getFibnoci1(int n) {
+    private static void printFibonacci1(int num) {
+        int x = 0, y = 1, z=0;
+        for (int i = 0 ; i < num; i++){
+            System.out.println(x);
+            z  =  x + y;
+            x = y;
+            y = z;
+        }
+    }
+
+    public static int printFibonacci2(int n) {
 
         if(n <= 1)
             return n;
-
-        //System.out.println(n);
-
-        return getFibnoci1(n-1) + getFibnoci1(n-2);
+        return printFibonacci2(n-1) + printFibonacci2(n-2);
     }
 }
 //
