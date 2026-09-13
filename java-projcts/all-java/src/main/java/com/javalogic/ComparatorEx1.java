b9a2e311c1718212cc0fea5ba03284565668fcb5
Fakhar MUmtaz <fakharmumtaz@gmail.com>
2026-09-13 18:27:49 -0400

    different java programs are committed

--- java-projcts/all-java/src/main/java/com/javalogic/ComparatorEx1.java
+++ java-projcts/all-java/src/main/java/com/javalogic/ComparatorEx1.java
@@ -1,8 +1,8 @@
 package com.javalogic;
 
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import com.javalogic.model.Product;
+
+import java.util.*;
 
 public class ComparatorEx1 {
     public static void main(String[] args) {
@@ -20,5 +20,20 @@ public class ComparatorEx1 {
 
         // Output will be ordered by key length: Cat, Dog, Giraffe, Elephant
         System.out.println(sortedMap);
+
+        //Product p = new Product(1111, "Prod1", 2.5);
+        List<Product> productList = List.of(new Product(2, "K", 5.51),
+                                            new Product(4, "C", 45.52),
+                                            new Product(3, "G", 25.54),
+                                            new Product(6, "E", 35.55),
+                                            new Product(1, "W", 6.59)
+                                          );
+                //;;new ArrayList<>();
+        productList.stream().filter(p -> p.getId() > 0).forEach(System.out::println);
+        boolean b = productList.stream().allMatch(s -> s.getId() == 1);
+        System.out.println(b);
+        boolean b1 = productList.stream().anyMatch(a -> a.getId() == 20);
+        System.out.println(b1);
+
     }
 }
--- java-projcts/all-java/src/main/java/com/javalogic/model/Product.java
+++ java-projcts/all-java/src/main/java/com/javalogic/model/Product.java
@@ -1,13 +1,11 @@
 package com.javalogic.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Setter
-@Getter
 @AllArgsConstructor
-class Product {
+@Data
+public class Product {
     private int id;
     private String name;
     private double price;
