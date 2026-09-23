e05662442481e342bff2a0543372a34e7a436bc0
Fakhar MUmtaz <fakharmumtaz@gmail.com>
2026-09-22 20:33:42 -0400

    different java programs are committed

---
+++ java-projcts/all-java/src/main/java/com/javalogic/str/LongestPalindrome.java
@@ -0,0 +1,22 @@
+package com.javalogic.str;
+
+import java.util.HashMap;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        String s = "abaccccddeee";
+
+        Stream.of(s.toCharArray()).forEach(System.out::print);
+        System.out.println();
+
+        int res = 0;
+        int oddCount = 0;
+        for (char ch : s.toCharArray() ){
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
+        }
+        System.out.println(freqMap);
+    }
+}
